3d16b1a63d294bcf95ae27640ec7fbe0348ced8f
Yaren Over <yaren.over@ynov.com>
2016-03-31 17:31:29 +0200

    Affichage article et article détaillé

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
-import {Page4} from './pages/page4/page4';
+import {Menu} from './pages/desserts/menu';
 import {HTTP_PROVIDERS} from 'angular2/http'
-import * as rx from 'rxjs'
 
 
 @App({
@@ -17,7 +15,7 @@ export class MyApp {
   }
 
   constructor(platform) {
-    this.rootPage = Page4;
+    this.rootPage = Menu;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
