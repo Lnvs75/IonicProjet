d00886e370f637f343b98541fa11c80b7c0c8675
qkab78 <q.kabasele@gmail.com>
2016-03-31 13:15:28 +0200

    ajout de boutons pour 'lire la suite'sur les cartes

--- app/pages/entrees/menu.js
+++ app/pages/entrees/menu.js
@@ -2,6 +2,7 @@
  * Created by Quentin on 31/03/2016.
  */
 import {Component} from 'angular2/core'
+import {IONIC_DIRECTIVES} from 'ionic-angular';
 
 @Component({
     selector: 'menu',
@@ -17,6 +18,10 @@ import {Component} from 'angular2/core'
       The most popular industrial group ever, and largely
       responsible for bringing the music to a mass audience.
     </p>
+    <button primary>
+        <ion-icon name="book"></ion-icon>
+        Lire la Suite
+    </button>
   </ion-card-content>
 </ion-card>
 
@@ -30,10 +35,49 @@ import {Component} from 'angular2/core'
       The most popular industrial group ever, and largely
       responsible for bringing the music to a mass audience.
     </p>
+    <button primary>
+        <ion-icon name="book"></ion-icon>
+        Lire la Suite
+    </button>
   </ion-card-content>
 </ion-card>
 
-    `
+<ion-card class="card">
+  <img src="Menu/Entrees/plats/soupe1.jpg" height="175"/>
+  <ion-card-content>
+    <ion-card-title>
+        Soupe de pois cassés
+      </ion-card-title>
+    <p>
+      The most popular industrial group ever, and largely
+      responsible for bringing the music to a mass audience.
+    </p>
+    <button primary>
+        <ion-icon name="book"></ion-icon>
+        Lire la Suite
+    </button>
+  </ion-card-content>
+</ion-card>
+
+<ion-card class="card">
+  <img src="Menu/Entrees/plats/salade2.jpg" height="175"/>
+  <ion-card-content>
+    <ion-card-title>
+        Salade d’endives aux oranges et aux noix
+      </ion-card-title>
+    <p>
+      The most popular industrial group ever, and largely
+      responsible for bringing the music to a mass audience.
+    </p>
+    <button primary>
+        <ion-icon name="book"></ion-icon>
+        Lire la Suite
+    </button>
+  </ion-card-content>
+</ion-card>
+
+    `,
+    directives : [IONIC_DIRECTIVES]
 })
 
 export class Menu{}
