f1631cb148154d821f6457a209df968a8cc7745b
qkab78 <q.kabasele@gmail.com>
2016-04-01 14:58:54 +0200

    redirection part2

--- app/pages/page2/menuAria.js
+++ app/pages/page2/menuAria.js
@@ -1,6 +1,6 @@
-import {Page} from 'ionic-angular'
-import {MenuEntree} from './pages/entrees/menuEntree'
-import {MenuDessert} from './pages/desserts/menuDessert';
+import {Page, NavController} from 'ionic-angular'
+import {MenuEntree} from '../entrees/menuEntree'
+import {MenuDessert} from '../desserts/menuDessert';
 
 @Page({
 	template:`
@@ -14,6 +14,7 @@ import {MenuDessert} from './pages/desserts/menuDessert';
 		</ion-card-header>
 		<ion-card-content class="contentcolor">
 			Une série d'entrée pour gourmets ou fan de plats rapides et succulents
+			<button (click)="turnToEntree()">Consulter les entrées!</button>
 		</ion-card-content>
 	</ion-card>
 	<ion-card class="mycardparams repas">
@@ -30,6 +31,7 @@ import {MenuDessert} from './pages/desserts/menuDessert';
 		</ion-card-header>
 		<ion-card-content class="contentcolor">
 			Personne n'a le temps pour faire attention au calories
+			<button (click)="turnToDessert()">Consulter les desserts!</button>
 		</ion-card-content>
 	</ion-card>
 </ion-content>
