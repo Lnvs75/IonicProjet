f7f48aef6667c7d3b374f1093e8cfdf18c568720
Lnvs75 <loic.neves@ynov.com>
2016-04-01 12:29:38 +0200

    HomePage

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,9 +1,11 @@
 import {Page} from 'ionic-angular';
 //import {Page2} from '../page2/page2';
+import{App} from './app'
 
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  directives:[App],
 })
 export class Home {
   
