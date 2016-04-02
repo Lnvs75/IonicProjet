2b97f3a657f14b2078039cbae83c1830817eb5cb
Lnvs75 <loic.neves@ynov.com>
2016-04-03 00:43:31 +0200

    HomePage  + Menu  Fixed

--- app/pages/home/app.js
+++ app/pages/home/app.js
@@ -1,28 +1,12 @@
 import {Component} from 'angular2/core'
 import {NavButton} from '../page1/navButton'
+import {IONIC_DIRECTIVES} from 'ionic-angular';
 
 @Component({
     selector: 'app',
     template: `
-    <ion-navbar *navbar hideBackButton>
-
-  <button menuToggle>
-    <ion-icon name='menu'></ion-icon>
-  </button>
-
-  <ion-title>
-    Menus
-  </ion-title>
-
-</ion-navbar>
-
-
-<ion-content padding>
-  <button block menuToggle>Toggle Menu</button>
-</ion-content>
-<nav-button></nav-button>
     `,
-    directives: [NavButton]
+    directives: [NavButton, IONIC_DIRECTIVES]
 })
 
 export class App{
